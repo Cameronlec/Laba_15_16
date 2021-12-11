0186c92d9d47d505830584c1d99adcbae5d186d7
Cameron <lecourtcam2gmail.com>
2021-12-11 14:30:47 +0300

    добавлен класс MenuItem

---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,20 @@
+package restaurant;
+
+public class MenuItem {
+
+    private int cost;
+    private String name;
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
