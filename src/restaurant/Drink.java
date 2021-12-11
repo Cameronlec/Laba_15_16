e681a34daf36481a8c5cdd2b6ec8abff0c66e0ae
Cameron <lecourtcam2gmail.com>
2021-12-11 14:47:19 +0300

    Исправлен Drink

--- src/restaurant/Drink.java
+++ src/restaurant/Drink.java
@@ -1,40 +1,40 @@
 package restaurant;
 
-public final class Drink implements Item
+public final class Drink extends MenuItem
 {
-    private float price;
-    private String name;
-    private String description;
+//    private float price;
+//    private String name;
+//    private String description;
 
     Drink(String name, String description)
     {
         if (name==null||name.equals("")||description==null||description.equals("")){
             throw new IllegalArgumentException("Неверные данные для нового блюда");
         }
-        this.price=0;
+        this.cost=0;
         this.name=name;
         this.description=description;
     }
 
-    Drink(float price, String name, String description)
+    Drink(int price, String name, String description)
     {
         if (price<0 || name==null||name.equals("")||description==null||description.equals("")){
             throw new IllegalArgumentException("Неверные данные для нового блюда");
         }
-        this.price=price;
+        this.cost=price;
         this.name=name;
         this.description=description;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getCost() {
-        return price;
-    }
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getCost() {
+//        return price;
+//    }
 }
